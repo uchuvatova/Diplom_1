887994d7163dd86df4113625ff38157aa459ab66
Ira Uchuvatova <i.uchuvatova@yandex.ru>
2022-10-04 22:41:29 +0300

    Добавлены тесты для класса Ingredient

---
+++ src/test/java/IngredientTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import praktikum.Ingredient;
+import praktikum.IngredientType;
+
+import static org.junit.Assert.assertEquals;
+import static praktikum.IngredientType.SAUCE;
+
+
+public class IngredientTest {
+    @Test
+    public void getNameShouldReturnName() {
+        Ingredient ingredient = new Ingredient(SAUCE, "hot sauce", 200);
+        String expected = "hot sauce";
+        String actual = ingredient.getName();
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void getPriceShouldReturnPrice() {
+        Ingredient ingredient = new Ingredient(SAUCE, "hot sauce", 200);
+        float expected = 200;
+        float actual = ingredient.getPrice();
+        assertEquals(expected, actual, 0.0f);
+    }
+
+    @Test
+    public void getTypeShouldReturnType() {
+        Ingredient ingredient = new Ingredient(SAUCE, "hot sauce", 200);
+        IngredientType actual = ingredient.getType();
+        assertEquals(SAUCE, actual);
+    }
+}
+
